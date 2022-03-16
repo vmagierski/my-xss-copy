749c331834f121c4267e7cdd3ae632e99fdd7e0a
Victor Magierski <magierski.v@husky.neu.edu>
2022-03-15 20:39:29 -0400

    new

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template_string, render_template, request
 import db
 
 app = Flask(__name__)
@@ -16,3 +16,9 @@ def index():
     return render_template('index.html',
                            comments=comments,
                            search_query=search_query)
+
+@app.route("/ssti", methods=['GET'])
+def home():
+    name = request.args.get('name') or None; # get untrusted query param
+    #return render_template_string(name); # render it into template
+    return 'hi ' + name;
